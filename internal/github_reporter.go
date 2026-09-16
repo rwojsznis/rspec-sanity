888d638f2692bd40fc7da4cd44947bfd1ab87b0b
Rafał W <wojsznis@pm.me>
2026-09-16 12:33:00 +0000

    fix(deps): update module github.com/google/go-github/v91 to v92

--- internal/github_reporter.go
+++ internal/github_reporter.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/google/go-github/v91/github"
+	"github.com/google/go-github/v92/github"
 	"golang.org/x/oauth2"
 )
 
--- internal/github_reporter_test.go
+++ internal/github_reporter_test.go
@@ -6,7 +6,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/google/go-github/v91/github"
+	"github.com/google/go-github/v92/github"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
